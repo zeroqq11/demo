9a5c3614ba0770d3806851ca23d32d64f84b0e48
liu <liujunyong@aitfinance.cn>
2018-06-20 16:21:19 +0800

    hahaha

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     public Greeting greeting() {
         Greeting g = new Greeting();
         g.setId(1);
-        g.setContent("jenkins auto build test! hahahah");
+        g.setContent("jenkins auto build test! hahahaha");
         return g;
     }
 }
