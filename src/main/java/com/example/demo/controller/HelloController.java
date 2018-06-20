591a248df56290573baa368d4101078e1627a5a5
liu <liujunyong@aitfinance.cn>
2018-06-20 10:24:32 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Greeting greeting() {
+        Greeting g = new Greeting();
+        g.setId(1);
+        g.setContent("hello world!");
+        return g;
+    }
+}
